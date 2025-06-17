858bbc8031c359804003ad0acb10cd88eec80949
Sumanasree1102 <sumanasreegundam1@gmail.com>
2025-06-17 09:48:27 +0530

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,37 @@
+class Node {
+    int data;
+    Node left, right;
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+class BinaryTree {
+    Node root;
+    void inorderTraversal(Node node) {
+        if (node != null) {
+            inorderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inorderTraversal(node.right);
+        }
+    }
+    void postorderTraversal(Node node) {
+        if (node != null) {
+            postorderTraversal(node.left);
+            postorderTraversal(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        System.out.println("Inorder traversal:");
+        tree.inorderTraversal(tree.root);
+        System.out.println("\nPostorder traversal:");
+        tree.postorderTraversal(tree.root);
+    }
+}
